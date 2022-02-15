b166d2e7ef69d1386906bb38b161c297695319a0
jun <72jiajunWu@gmail.com>
2022-02-15 22:22:25 +0100

    fix menu problem

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 </div>
+</div>
 <footer>
     <div class="container footer-block">
         <p class="footer"> © 2022 - Tous droits réservés </p>
--- header.php
+++ header.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>">
+<html lang="<?php
+
+use function DI\add;
+
+ language_attributes(); ?>">
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
@@ -9,48 +13,116 @@
   <title>Chill Time</title>
 </head>
 
+<?php 
+  function getLists(){
+    $args = [
+      'post_parent'=>0,
+      'post_type'=>'page',
+    ];
+    $my_pages = get_posts($args);
+    $list = [];
+    foreach ($my_pages as $page){
+      $args = [
+        'post_parent'=>$page->ID,
+        'post_type'=>'page',
+        'meta_query'=>[
+          
+            'key'=>"menu_order",
+            'value'=>'0',
+            'compare'=>">"
+          
+        ]
+      ];
+      $subPages = get_posts($args);
+      // if (count($subPages)>0){
+      //   var_dump($subPages);
+      // }
+      // var_dump($subPages);
+      $children=[];
+      foreach($subPages as $subPage){
+
+        array_push($children, [
+          "name"=>$subPage->post_name,
+          "url"=>$subPage->guid,
+        ]);
+        
+      }
+      // var_dump($children);
+      
+      $object = [
+        "parent"=>[
+          "url"=>$page->guid,
+          "name"=>$page->post_name
+        ],
+        "children"=>$children,
+      ];
+      array_push($list, $object);
+    }
+    // die();
+    // var_dump($list);
+    // die();
+    return $list;
+    
+  }
+  $list = getLists();
+  getLists();
+?>
+
 <body>
-
-
+  
   <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: <?= get_option('footer_background'); ?>!important">
-    <div class="container-fluid">
+    <!-- <div class="container-fluid"> -->
       <a class="navbar-brand" href="<?php bloginfo('url') ?>">
-        <!-- <?php bloginfo('name') ?> -->
         <?php
-        $custom_logo_id = get_theme_mod('custom_logo');
-        $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+          $custom_logo_id = get_theme_mod('custom_logo');
+          $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
 
-        if (has_custom_logo()) {
-          echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
-        } else {
-          echo '<h4>' . get_bloginfo('name') . '</h4>';
-        }
-        ?>
-      </a>
+          if (has_custom_logo()) {
+            echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+          } else {
+            echo '<h4>' . get_bloginfo('name') . '</h4>';
+          }
+          ?>
+        </a>
 
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <?php foreach($list as $i=>$page){ ?>
+            
+          <li class="nav-item <?= $i==0 ? 'active' : '' ?>" <?= count($page['children']) > 0 ? "dropdown" : "" ?> >
+            
+            <a  class="nav-link <?= count($page['children']) > 0 ? "dropdown-toggle" : "" ?>" 
+                href="<?= $page->guid ?>"
+                data-bs-toggle="<?= count($page['children']) > 0 ? 'collapse':'' ?>"
+                data-bs-target="<?= count($page['children']) > 0 ? '#dropdown-submenu':'' ?>"
+                aria-controls="<?= count($page['children']) > 0 ? '#dropdown-submenu':'' ?>"
+            >
+              <?= $page['parent']['name'] ?> 
+            </a>
+            
+            <?php if (count($page['children']) > 0) : ?>
+            <div class="dropdown-menu" aria-labelledby="dropdown-submenu" id="dropdown-submenu">
+              <?php foreach($page['children'] as $j=>$subPage){ ?>
+                
+                  <a class="dropdown-item" href="#">Action</a>
+                
+              <?php } ?>
+            </div>
+            <?php endif; ?>
+            
+          </li>
+          <?php }?>
+        </ul>
 
-
-        <?php
-
-        wp_nav_menu([
-          'theme_location'    => 'header',
-          'container'         => false,
-          'menu_class'        => 'navbar-nav me-auto mb-2 mb-lg-0',
-          'depth'             => 2,
-        ]);
-
-
-        ?>
-
-        <?= get_search_form() ?>
       </div>
-    </div>
+    <!-- </div> -->
   </nav>
-
+  
+</body>
+  <?= get_search_form() ?>
   <div class="global_container">
     <div class="container">
